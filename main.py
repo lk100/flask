efb3425671a8850ec52d598877bc13fc4cfab4c1
lk100 <91986329+lk100@users.noreply.github.com>
2025-04-27 18:15:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 from transformers import pipeline
+import os 
 
 # Load the Hugging Face model
 classifier = pipeline(
@@ -35,4 +36,5 @@ def submit_journal():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set by Railway
+    app.run(debug=True, host="0.0.0.0", port=port)
